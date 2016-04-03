33a8c30eba1902eb74940d3498856d09ebb2deb3
William Stevens <contact@wastevensv.com>
2016-04-03 04:27:21 -0400

    Can draw axis on top of display.

--- combine-test.cpp
+++ combine-test.cpp
@@ -3,6 +3,7 @@
 #include <GLFW/glfw3.h>
 #include <SOIL/SOIL.h>
 
+
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
@@ -46,16 +47,16 @@ float bg_vertices[] = {
      0.0f, -1.0f,  0.0f, 0.0f, 0.0f, // Bottom Left
 };
 
-float axis_verticies[] = {
+float axis_vertices[] = {
 // Position              Color
      0.0f,  0.0f,  0.0f, 1.0f, 0.0f,  0.0f, // Origin
-     1.0f,  0.0f,  0.0f, 1.0f, 0.0f,  0.0f, // X Line
+     0.5f,  0.0f,  0.0f, 1.0f, 0.0f,  0.0f, // X Line
     
      0.0f,  0.0f,  0.0f, 0.0f, 1.0f,  0.0f, // Origin
-     0.0f,  1.0f,  0.0f, 0.0f, 1.0f,  0.0f, // Y Line
+     0.0f,  0.5f,  0.0f, 0.0f, 1.0f,  0.0f, // Y Line
     
      0.0f,  0.0f,  0.0f, 0.0f, 0.0f,  1.0f, // Origin
-     0.0f,  0.0f,  1.0f, 0.0f, 0.0f,  1.0f, // Z Line
+     0.0f,  0.0f,  0.5f, 0.0f, 0.0f,  1.0f, // Z Line
 };
 
 std::string readFile(const char *filePath) {
@@ -146,31 +147,80 @@ int main()
     glVertexAttribPointer(TEX_ID, 2, GL_FLOAT, GL_FALSE,
                  5*sizeof(GLfloat), (void*)(3*sizeof(GLfloat)));
 
+    // Create Axis Vertex Array Object
+    GLuint axis_vao;
+    glGenVertexArrays(1, &axis_vao);
+    glBindVertexArray(axis_vao);
+
+    // Create Axis Vertex Buffer Object
+    GLuint axis_vbo;
+    glGenBuffers(1, &axis_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, axis_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(axis_vertices), axis_vertices, GL_STATIC_DRAW);
+    
+    glEnableVertexAttribArray(POS_ID);
+    glVertexAttribPointer(POS_ID, 3, GL_FLOAT, GL_FALSE,
+                 6*sizeof(GLfloat), 0);
+
+    glEnableVertexAttribArray(TEX_ID);
+    glVertexAttribPointer(TEX_ID, 3, GL_FLOAT, GL_FALSE,
+                 6*sizeof(GLfloat), (void*)(3*sizeof(GLfloat)));
+
 
     // Compile color vertex shader
-    GLuint prettyVert = glCreateShader(GL_VERTEX_SHADER);
-    const char *vertexSource = readFile("3dvert.glsl").c_str();
+    GLuint colorVert = glCreateShader(GL_VERTEX_SHADER);
+    const char *cvSource = readFile("3dvert.glsl").c_str();
 
-    glShaderSource(prettyVert, 1, &vertexSource, NULL);
-    glCompileShader(prettyVert);
+    glShaderSource(colorVert, 1, &cvSource, NULL);
+    glCompileShader(colorVert);
     GLint cvStatus;
-    glGetShaderiv(prettyVert, GL_COMPILE_STATUS, &cvStatus);
+    glGetShaderiv(colorVert, GL_COMPILE_STATUS, &cvStatus);
     if(cvStatus != GL_TRUE) {
+        char buffer[512];
+        glGetShaderInfoLog(colorVert, 512, NULL, buffer);
+        std::cerr << buffer << std::endl;
+        return 3;
+    }
+
+    // Compile color fragment shader
+    GLuint colorFrag = glCreateShader(GL_FRAGMENT_SHADER);
+    const char* cfSource = readFile("colorfrag.glsl").c_str();
+
+    glShaderSource(colorFrag, 1, &cfSource, NULL);
+    glCompileShader(colorFrag);
+    GLint cfStatus;
+    glGetShaderiv(colorFrag, GL_COMPILE_STATUS, &cfStatus);
+    if(cfStatus != GL_TRUE) {
+        char buffer[512];
+        glGetShaderInfoLog(colorFrag, 512, NULL, buffer);
+        std::cerr << buffer << std::endl;
+        return 4;
+    }
+
+    // Compile pretty vertex shader
+    GLuint prettyVert = glCreateShader(GL_VERTEX_SHADER);
+    const char *pvSource = readFile("3dvert.glsl").c_str();
+
+    glShaderSource(prettyVert, 1, &pvSource, NULL);
+    glCompileShader(prettyVert);
+    GLint pvStatus;
+    glGetShaderiv(prettyVert, GL_COMPILE_STATUS, &pvStatus);
+    if(pvStatus != GL_TRUE) {
         char buffer[512];
         glGetShaderInfoLog(prettyVert, 512, NULL, buffer);
         std::cerr << buffer << std::endl;
         return 3;
     }
 
-    // Compile color fragment shader
+    // Compile pretty fragment shader
     GLuint prettyFrag = glCreateShader(GL_FRAGMENT_SHADER);
-    const char* fragmentSource = readFile("prettyfrag.glsl").c_str();
+    const char* pfSource = readFile("prettyfrag.glsl").c_str();
 
-    glShaderSource(prettyFrag, 1, &fragmentSource, NULL);
+    glShaderSource(prettyFrag, 1, &pfSource, NULL);
     glCompileShader(prettyFrag);
-    GLint fStatus;
-    glGetShaderiv(prettyFrag, GL_COMPILE_STATUS, &fStatus);
-    if(fStatus != GL_TRUE) {
+    GLint pfStatus;
+    glGetShaderiv(prettyFrag, GL_COMPILE_STATUS, &pfStatus);
+    if(pfStatus != GL_TRUE) {
         char buffer[512];
         glGetShaderInfoLog(prettyFrag, 512, NULL, buffer);
         std::cerr << buffer << std::endl;
@@ -179,9 +229,9 @@ int main()
 
     // Compile blank vertex shader
     GLuint blankVert = glCreateShader(GL_VERTEX_SHADER);
-    const char *blankVertexSource = readFile("3dvert.glsl").c_str();
+    const char *bvSource = readFile("3dvert.glsl").c_str();
 
-    glShaderSource(blankVert, 1, &blankVertexSource, NULL);
+    glShaderSource(blankVert, 1, &bvSource, NULL);
     glCompileShader(blankVert);
     GLint bvStatus;
     glGetShaderiv(blankVert, GL_COMPILE_STATUS, &bvStatus);
@@ -194,25 +244,34 @@ int main()
 
     // Compile blank fragment shader
     GLuint blankFrag = glCreateShader(GL_FRAGMENT_SHADER);
-    fragmentSource = readFile("simplefrag.glsl").c_str();
+    const char* bfSource = readFile("simplefrag.glsl").c_str();
 
-    glShaderSource(blankFrag, 1, &fragmentSource, NULL);
+    glShaderSource(blankFrag, 1, &bfSource, NULL);
     glCompileShader(blankFrag);
-    glGetShaderiv(blankFrag, GL_COMPILE_STATUS, &fStatus);
-    if(fStatus != GL_TRUE) {
+    GLint bfStatus;
+    glGetShaderiv(blankFrag, GL_COMPILE_STATUS, &bfStatus);
+    if(bfStatus != GL_TRUE) {
         char buffer[512];
         glGetShaderInfoLog(blankFrag, 512, NULL, buffer);
         std::cerr << buffer << std::endl;
         return 5;
     }
 
-    // Make blank shader program.
+    // Make color shader program.
     GLuint colorShaderProgram = glCreateProgram();
-    glAttachShader(colorShaderProgram, prettyVert);
-    glAttachShader(colorShaderProgram, prettyFrag);
+    glAttachShader(colorShaderProgram, colorVert);
+    glAttachShader(colorShaderProgram, colorFrag);
     glBindFragDataLocation(colorShaderProgram, 0, "outColor");
     glLinkProgram(colorShaderProgram);
     glUseProgram(colorShaderProgram);
+    
+    // Make pretty shader program.
+    GLuint prettyShaderProgram = glCreateProgram();
+    glAttachShader(prettyShaderProgram, prettyVert);
+    glAttachShader(prettyShaderProgram, prettyFrag);
+    glBindFragDataLocation(prettyShaderProgram, 0, "outColor");
+    glLinkProgram(prettyShaderProgram);
+    glUseProgram(prettyShaderProgram);
     
     // Make blank shader program.
     GLuint blankShaderProgram = glCreateProgram();
@@ -231,13 +290,17 @@ int main()
     glm::mat4 proj = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 1.0f, 10.0f);
     glm::mat4 model = glm::mat4();
 
-    glUniformMatrix4fv(glGetUniformLocation(blankShaderProgram, "view" ), 1, GL_FALSE, glm::value_ptr(view));
-    glUniformMatrix4fv(glGetUniformLocation(blankShaderProgram, "proj" ), 1, GL_FALSE, glm::value_ptr(proj));
-    glUniformMatrix4fv(glGetUniformLocation(blankShaderProgram, "model"), 1, GL_FALSE, glm::value_ptr(model));
-
     glUniformMatrix4fv(glGetUniformLocation(colorShaderProgram, "view" ), 1, GL_FALSE, glm::value_ptr(view));
     glUniformMatrix4fv(glGetUniformLocation(colorShaderProgram, "proj" ), 1, GL_FALSE, glm::value_ptr(proj));
     glUniformMatrix4fv(glGetUniformLocation(colorShaderProgram, "model"), 1, GL_FALSE, glm::value_ptr(model));
+
+    glUniformMatrix4fv(glGetUniformLocation(prettyShaderProgram, "view" ), 1, GL_FALSE, glm::value_ptr(view));
+    glUniformMatrix4fv(glGetUniformLocation(prettyShaderProgram, "proj" ), 1, GL_FALSE, glm::value_ptr(proj));
+    glUniformMatrix4fv(glGetUniformLocation(prettyShaderProgram, "model"), 1, GL_FALSE, glm::value_ptr(model));
+    
+    glUniformMatrix4fv(glGetUniformLocation(blankShaderProgram, "view" ), 1, GL_FALSE, glm::value_ptr(view));
+    glUniformMatrix4fv(glGetUniformLocation(blankShaderProgram, "proj" ), 1, GL_FALSE, glm::value_ptr(proj));
+    glUniformMatrix4fv(glGetUniformLocation(blankShaderProgram, "model"), 1, GL_FALSE, glm::value_ptr(model));
 
     // Create texture
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
@@ -269,6 +332,26 @@ int main()
 
         glm::mat4 model;
         
+        // Draw Axis
+        glUseProgram(colorShaderProgram);
+        glUniformMatrix4fv(glGetUniformLocation(prettyShaderProgram, "view"), 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(glGetUniformLocation(prettyShaderProgram, "proj"), 1, GL_FALSE, glm::value_ptr(proj));
+        glUniformMatrix4fv(glGetUniformLocation(prettyShaderProgram, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        
+        glBindVertexArray(axis_vao);
+        glDrawArrays(GL_LINES, 0, 6);
+        glBindVertexArray(0);
+        
+        // Draw Backboard
+        glUseProgram(prettyShaderProgram);
+        glUniformMatrix4fv(glGetUniformLocation(prettyShaderProgram, "view"), 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(glGetUniformLocation(prettyShaderProgram, "proj"), 1, GL_FALSE, glm::value_ptr(proj));
+        glUniformMatrix4fv(glGetUniformLocation(prettyShaderProgram, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        
+        glBindVertexArray(bg_vao);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glBindVertexArray(0);
+        
         // Draw Baseboard
         glUseProgram(blankShaderProgram);
         glUniformMatrix4fv(glGetUniformLocation(blankShaderProgram, "view"), 1, GL_FALSE, glm::value_ptr(view));
@@ -279,16 +362,6 @@ int main()
         glDrawArrays(GL_TRIANGLES, 0, 6);
         glBindVertexArray(0);
 
-        // Draw Backboard
-        glUseProgram(colorShaderProgram);
-        glUniformMatrix4fv(glGetUniformLocation(colorShaderProgram, "view"), 1, GL_FALSE, glm::value_ptr(view));
-        glUniformMatrix4fv(glGetUniformLocation(colorShaderProgram, "proj"), 1, GL_FALSE, glm::value_ptr(proj));
-        glUniformMatrix4fv(glGetUniformLocation(colorShaderProgram, "model"), 1, GL_FALSE, glm::value_ptr(model));
-        
-        glBindVertexArray(bg_vao);
-        glDrawArrays(GL_TRIANGLES, 0, 6);
-        glBindVertexArray(0);
-        
         //Display
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -296,17 +369,23 @@ int main()
 
     // --- Cleanup/Shutdown ---
     glDeleteProgram(colorShaderProgram);
+    glDeleteProgram(prettyShaderProgram);
     glDeleteProgram(blankShaderProgram);
     
+    glDeleteShader(colorFrag);
+    glDeleteShader(colorVert);
+
     glDeleteShader(prettyFrag);
     glDeleteShader(prettyVert);
 
     glDeleteShader(blankVert);
     glDeleteShader(blankFrag);
 
+    glDeleteBuffers(1, &axis_vbo);
     glDeleteBuffers(1, &fg_vbo);
     glDeleteBuffers(1, &bg_vbo);
 
+    glDeleteVertexArrays(1, &axis_vao);
     glDeleteVertexArrays(1, &fg_vao);
     glDeleteVertexArrays(1, &bg_vao);
     
@@ -314,4 +393,3 @@ int main()
     glfwTerminate();
     return 0;
 }
-
